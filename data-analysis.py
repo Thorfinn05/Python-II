ed2d6a0266db9521a447becb19bb324def2ffc10
Rudranil Das <rudranild04@gmail.com>
2025-04-06 12:20:31 +0530

    Some Visual views of data

--- data-analysis.py
+++ data-analysis.py
@@ -10,3 +10,12 @@ print(iris.shape)
 print(iris.columns)
 print(iris['variety'].value_counts())
 print(iris.info())
+
+#First Plot
+plt.plot(iris['variety'])
+plt.xlabel("No. of data points")
+plt.show()
+
+#Second Plot
+plt.hist(iris["variety"],color="green")
+plt.show()
