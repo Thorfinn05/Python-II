8d10cc96c00726003c9dc7bb3ce0aeea526af3ed
Rudranil Das <rudranild04@gmail.com>
2025-04-06 12:37:45 +0530

    Scatter plot

--- data-analysis.py
+++ data-analysis.py
@@ -5,19 +5,24 @@ import seaborn as sb
 
 iris = pd.read_csv("C:\\Users\\User\\Downloads\\iris.csv")
 
-print(iris.head())
-print(iris.shape)
-print(iris.columns)
-print(iris['variety'].value_counts())
-print(iris.info())
+# print(iris.head())
+# print(iris.shape)
+# print(iris.columns)
+# print(iris['variety'].value_counts())
+# print(iris.info())
 
-#First Plot
-plt.plot(iris['variety'])
-plt.xlabel("No. of data points")
+# #First Plot
+# plt.plot(iris['variety'])
+# plt.xlabel("No. of data points")
+# plt.show()
+
+# #Second Plot
+# plt.hist(iris["variety"],color="green")
+# plt.show()
+
+# print(iris.describe())
+
+sb.set_style('whitegrid')
+sb.scatterplot(data=iris, x='sepal.length', y='sepal.width', hue='variety')
+plt.plot()
 plt.show()
-
-#Second Plot
-plt.hist(iris["variety"],color="green")
-plt.show()
-
-print(iris.describe())
