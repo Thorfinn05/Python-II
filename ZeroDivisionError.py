cd89e4063ab27c5a2a486b0c70d512846710fb05
Rudranil Das <rudranild04@gmail.com>
2025-06-15 10:38:16 +0530

    Fix input handling by moving user prompt inside try block

--- ZeroDivisionError.py
+++ ZeroDivisionError.py
@@ -3,8 +3,9 @@
 # except ZeroDivisionError as e:
 #     print('ZeroDivisionError caught:', e)
 
-a = int(input("Enter a number: "))
+
 try: 
+    a = int(input("Enter a number: "))
     num = int(input("Enter a number to divide " + str(a) + " by:"))
     result = a/num
     print("Result: ", result)
