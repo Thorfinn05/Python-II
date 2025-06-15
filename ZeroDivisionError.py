25f2d4b1fa3061aae4f0d10336579d268352d745
Rudranil Das <rudranild04@gmail.com>
2025-06-15 10:34:53 +0530

    Refactor ZeroDivisionError handling and improve user input prompts

--- ZeroDivisionError.py
+++ ZeroDivisionError.py
@@ -1,4 +1,14 @@
-try:
-    result = 10/0
+# try:
+#     result = 10/0
+# except ZeroDivisionError as e:
+#     print('ZeroDivisionError caught:', e)
+
+a = int(input("Enter a number: "))
+try: 
+    num = int(input("Enter a number to divide " + str(a) + " by:"))
+    result = a/num
+    print("Result: ", result)
 except ZeroDivisionError as e:
-    print('ZeroDivisionError caught:', e)
+    print("ZeroDivisionError caught:", e)
+except ValueError:
+    print("Invalid input! Please enter an integer")
