28f497d6e6ce30e207455261223f7a150c77c525
Rudranil Das <rudranild04@gmail.com>
2025-06-17 10:31:10 +0530

    Refactor input handling to remove eval usage for username and password

--- loginCredentials-csv.py
+++ loginCredentials-csv.py
@@ -4,8 +4,8 @@ f = open("loginCredentials.csv", "w", newline="")
 cwr = csv.writer(f)
 cwr.writerow(['Username', 'Password'])
 while True:
-    username = eval(input("Enter username: "))
-    password = eval(input("Enter password: "))
+    username = (input("Enter username: "))
+    password = (input("Enter password: "))
     cwr.writerow([username, password])
     ans = input('Enter y for more data: ')
     if(ans != 'y'):
